aef0ecd8f34123f8eb8ed7ffb64d91e0d0359fd5
KenLDubois <40549554+KenLDubois@users.noreply.github.com>
2018-09-19 23:53:32 -0400

    Add files via upload

---
+++ Outline text and Embed linked.jsx
@@ -0,0 +1,25 @@
+﻿// DUPLICATE and OUTLINE TEXT FRAMES
+
+var doc = app.activeDocument; 
+var iFrame = doc.textFrames;
+var frameLen = iFrame.length;
+var frameName = null;
+
+for(i=0; i <= frameLen - 1; i++){                                              // iterate through all frames
+        if(iFrame[i].contents !="" && iFrame[i].hidden!=true){      // ignor empty and hidden text frames
+            frameName = iFrame[i].name;
+            iFrame[i].duplicate();
+            iFrame[i].createOutline();
+            iFrame[i].hidden=true;
+        };
+    };
+
+// EMBED ALL PLACED IMAGES
+
+var doc = app.activeDocument; 
+var iPlaced = doc.placedItems;
+var placedLen = iPlaced.length;
+
+for(i=0; i<= placedLen - 1; i++){   
+    iPlaced[i].embed();
+    };
